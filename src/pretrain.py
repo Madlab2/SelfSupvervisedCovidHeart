134ac950557a507d3405623cad98aae1a2bb2cd9
Stevan Lezaja <stevan.lezaja@gmail.com>
2024-04-23 16:08:15 +0200

    pretrain simplification, untested

--- src/pretrain.py
+++ src/pretrain.py
@@ -10,20 +10,11 @@ import matplotlib.pyplot as plt
 from skimage.measure import label as skimage_label, regionprops
 
 import monai
-from monai.data import CacheDataset, DataLoader
+from monai.data import DataLoader, ArrayDataset
 from monai.networks.nets import UNet
 from monai.networks.utils import one_hot
 
-from monai.transforms import (
-    Compose,
-    CopyItemsd,
-    EnsureChannelFirstd,
-    FgBgToIndicesd,
-    LabelToMaskd,
-    RandCropByPosNegLabeld,
-    RandRotate90d,
-    RandAxisFlipd,
-)
+from monai.transforms import RandomCropd
 
 from config import *
 from model import *
@@ -40,66 +31,33 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 image = torch.from_numpy(np.load(join(DATA_PATH, 'train', 'data_0.npy'))).float()
 
 print("Noising image...")
-blurred_image = add_noise(image, sigma=1.0)
+blurred_image = add_noise(image)
 print("image noise.")
 
 # cut through the image for validation and train split
-cut_idx_z = np.floor(image.shape(-1)*0.7) # 70% split
+cut_idx_z = int(np.floor(image.shape[-1]*0.7)) # 70% split 716
 
+train_image = blurred_image[..., :cut_idx_z]
+train_label = image[..., :cut_idx_z]
 
-train_image = blurred_image[:, :, :cut_idx_z]
-train_label = image[:, :, :cut_idx_z]
+val_image = blurred_image[..., :cut_idx_z]
+val_label = image[..., cut_idx_z:]
 
-val_image = blurred_image[:, :, :cut_idx_z]
-val_label = image[:, :, cut_idx_z:]
-
-
-
-pretrain_transforms = Compose([
-    EnsureChannelFirstd(keys=['image', 'label'], channel_dim='no_channel'),
-    CopyItemsd(keys=['label'], times=1, names=['mask']),                                                  # Copy label to new image mask
-    LabelToMaskd(keys=['mask'], select_labels=[1, 2], merge_channels=True),                               # Convert mask to binary mask showing where labels are
-    FgBgToIndicesd(keys=['mask'], fg_postfix='_fg_indices', bg_postfix='_bg_indices'),                    # Precompute indices of labelled voxels
-    RandCropByPosNegLabeld(keys=['image', 'label', 'mask'], label_key='label', spatial_size=PATCH_SIZE,   # Extract random crop
-                            pos=PROB_FOREGROUND_CENTER, neg=(1.0 - PROB_FOREGROUND_CENTER),
-                            num_samples=1, fg_indices_key='mask_fg_indices', bg_indices_key='mask_bg_indices'),
-    RandRotate90d(keys=['image', 'label', 'mask'], prob=0.5, spatial_axes=(0, 1)),                        # Randomly rotate
-    RandRotate90d(keys=['image', 'label', 'mask'], prob=0.5, spatial_axes=(1, 2)),                        # Randomly rotate
-    RandRotate90d(keys=['image', 'label', 'mask'], prob=0.5, spatial_axes=(0, 2)),                        # Randomly rotate
-    RandAxisFlipd(keys=['image', 'label', 'mask'], prob=0.1),                                             # Randomly flip
-])
-
-pretrain_dataset = RepeatedCacheDataset(
-    data=[{'image': train_image, 'label': train_label}],
-    num_repeats=BATCHES_PER_EPOCHS * TRAIN_BATCH_SIZE,
-    transform=pretrain_transforms,
-    num_workers=0,
-    cache_rate=1.0,
-    copy_cache=False
-)
-
-preval_transforms = Compose([
-    EnsureChannelFirstd(keys=['image', 'label', 'mask'], channel_dim='no_channel'),
-])
-
-preval_dataset = CacheDataset(
-    data=extract_label_patches(val_image, val_label, PATCH_SIZE)[0:25],
-    transform=preval_transforms,
-    num_workers=0,
-    cache_rate=1.0
-)
+pretrain_dataset = ArrayDataset(train_image, train_label)
+preval_dataset = ArrayDataset(val_image, val_label)
 
 checkpoint = torch.load(convert_path('./models/worst_model_checkpoint.pth'), map_location=torch.device(DEVICE))
 model.load_state_dict(checkpoint['model'])
 
+cropper = RandomCropd(keys=['image', 'label'], roi_size=(96, 96, 96), random_size=False)
 
 train_loader = DataLoader(
     pretrain_dataset,
     batch_size=PRE_TRAIN_BATCH_SIZE,
     shuffle=False,  # Don't shuffle since we use random crops
-    num_workers=8,
-    persistent_workers=True,
+    num_workers=0,
     pin_memory=True,
+    collate_fn=cropper,
 )
 
 val_loader = DataLoader(
@@ -107,12 +65,12 @@ val_loader = DataLoader(
     batch_size=PRE_VAL_BATCH_SIZE,
     shuffle=False,
     ### YOUR CODE HERE ###
-    num_workers=8,
-    persistent_workers=True,
+    num_workers=0,
     pin_memory=True,
+    collate_fn=cropper,
 )
 
-model.todevice(DEVICE)
+model.to(DEVICE)
 
 print('Starting pretraining')
 all_train_losses = []
@@ -127,8 +85,6 @@ for epoch in range(PRE_NUM_EPOCHS):
     for batch in tqdm(train_loader):
         image_b = batch['image'].as_tensor().to(DEVICE, non_blocking=True)
         label = batch['label'].as_tensor().to(DEVICE, non_blocking=True)
-        label = one_hot(label, num_classes=3)
-        label = label[:, 1:]
 
         with torch.cuda.amp.autocast():     #### Probably will crash for CPU? ####
             pred = model(image_b)
@@ -160,8 +116,6 @@ for epoch in range(PRE_NUM_EPOCHS):
         label = batch['label'].as_tensor().to(DEVICE, non_blocking=True)
         
         with torch.no_grad():
-            label = one_hot(label, num_classes=3)
-            label = label[:, 1:]
 
             with torch.cuda.amp.autocast():     #### Probably will crash for CPU? ####
                 pred = model(image_b)
